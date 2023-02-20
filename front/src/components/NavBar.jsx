a54fbe042956a6f793d353a68ff41a2aa9aa42b3
Bruna Falcucci <brunapfalcucci@hotmail.com>
2023-02-20 17:26:37 -0300

    Create Front with react.js

---
+++ front/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavBar = () => {
+  return (
+    <Bruna> OLÁ PESSOAS </Bruna>
+  )
+}
+
+
+const Bruna = styled.div`
+font-size: 100px;
+color: purple;
+`
+
+export default NavBar
