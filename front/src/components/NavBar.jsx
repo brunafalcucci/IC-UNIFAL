2d3f6f923dcded800f8b78a82f301bfa2f564d5a
Bruna Falcucci <brunapfalcucci@hotmail.com>
2023-02-21 19:03:53 -0300

    create navbar

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import logo from '../img/LOGO-IC.png'
 const NavBar = () => {
   return (
     <WrapSide>  
-      Side Bar 
+      <img src={logo} alt="" />
+      <WrapButtons> 
+      <button> CRITICALITY INDEX </button>
+      <button> EFFICIENCY ENERGETIC </button>
+      <button> ALGUMA COISA </button>
+      <button> ALGUMA COISA </button>
+      <button> ALGUMA COISA </button>
+      </WrapButtons>
     </WrapSide>
   )
 }
 
 
 const WrapSide = styled.div`
-font-size: 100px;
-color: purple;
+width: 18%;
+height: 100%;
+min-height: 100vh;
+background-color: #D4F8B9; 
+img {
+  width: 90%;
+}
 `
+const WrapButtons = styled.div`
+display: flex;
+flex-direction: column;
+margin-left: 10%;
+width: 75%;
+button { 
+margin: 10%;
+border: 2px solid #525250;
+border-radius: 10px;
+font-size: 20px;
+background-color: #D4F8B9;
+box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
 
+}
+
+`
 export default NavBar
