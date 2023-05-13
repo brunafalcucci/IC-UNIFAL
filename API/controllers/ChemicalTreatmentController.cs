3408e55af449f1b0cc2f5ba41369bfd259e00b4f
Murilo Sousa Moraes <murilosousamoraes@gmail.com>
2023-05-13 05:15:28 -0300

    Creating the white water calculation

--- API/controllers/ChemicalTreatmentController.cs
+++ API/controllers/ChemicalTreatmentController.cs
@@ -60,76 +60,76 @@ namespace Application.Controllers
             }
         }
 
-        public string CalculateEnvironmentalRisk(double solubilityValue, double toxicityValue)
+        public string CalculateWhiteWater(double distilledWaterRecyclingValue, double reuseOfDistilledWaterInOtherApplicationsValue)
         {
-            LinguisticVariable solubility = new( "Solubility", 0, 33 );
-            solubility.AddLabel( new FuzzySet( "VeryInsoluble", new TrapezoidalFunction(0, 0, (float)3.3, (float)9.9) ) );
-            solubility.AddLabel( new FuzzySet( "Insoluble", new TrapezoidalFunction((float)3.3, (float)9.9, (float)16.5) ) );
-            solubility.AddLabel( new FuzzySet( "AverageSoluble", new TrapezoidalFunction((float)9.9, (float)16.5, (float)23.1) ) );
-            solubility.AddLabel( new FuzzySet( "Soluble", new TrapezoidalFunction((float)16.5, (float)23.1, (float)29.7) ) );
-            solubility.AddLabel( new FuzzySet( "VerySoluble", new TrapezoidalFunction((float)23.1, (float)29.7, 33, 33) ) );
+            LinguisticVariable distilledWaterRecycling = new( "DistilledWaterRecycling", 0, 10 );
+            distilledWaterRecycling.AddLabel( new FuzzySet( "VeryLow", new TrapezoidalFunction(0, 0, 1, 3) ) );
+            distilledWaterRecycling.AddLabel( new FuzzySet( "Low", new TrapezoidalFunction(1, 3, 5) ) );
+            distilledWaterRecycling.AddLabel( new FuzzySet( "Middle", new TrapezoidalFunction(3, 5, 7) ) );
+            distilledWaterRecycling.AddLabel( new FuzzySet( "High", new TrapezoidalFunction(5, 7, 9) ) );
+            distilledWaterRecycling.AddLabel( new FuzzySet( "VeryHigh", new TrapezoidalFunction(7, 9, 10, 10) ) );
 
-            LinguisticVariable toxicity = new( "Toxicity", 0, 45 );
-            toxicity.AddLabel( new FuzzySet( "VeryLow", new TrapezoidalFunction(0, 0, (float)4.5, (float)13.5) ) );
-            toxicity.AddLabel( new FuzzySet( "Low", new TrapezoidalFunction((float)4.5, (float)13.5, (float)22.5) ) );
-            toxicity.AddLabel( new FuzzySet( "Medium", new TrapezoidalFunction((float)13.5, (float)22.5, (float)31.5) ) );
-            toxicity.AddLabel( new FuzzySet( "High", new TrapezoidalFunction((float)22.5, (float)31.5, (float)40.5) ) );
-            toxicity.AddLabel( new FuzzySet( "VeryHigh", new TrapezoidalFunction((float)31.5, (float)40.5, 45, 45) ) );
+            LinguisticVariable reuseOfDistilledWaterInOtherApplications = new( "ReuseOfDistilledWaterInOtherApplications", 0, 10 );
+            reuseOfDistilledWaterInOtherApplications.AddLabel( new FuzzySet( "VeryLow", new TrapezoidalFunction(0, 0, 1, 3) ) );
+            reuseOfDistilledWaterInOtherApplications.AddLabel( new FuzzySet( "Low", new TrapezoidalFunction(1, 3, 5) ) );
+            reuseOfDistilledWaterInOtherApplications.AddLabel( new FuzzySet( "Medium", new TrapezoidalFunction(3, 5, 7) ) );
+            reuseOfDistilledWaterInOtherApplications.AddLabel( new FuzzySet( "High", new TrapezoidalFunction(5, 7, 9) ) );
+            reuseOfDistilledWaterInOtherApplications.AddLabel( new FuzzySet( "VeryHigh", new TrapezoidalFunction(7, 9, 10, 10) ) );
 
-            LinguisticVariable environmentalRisk = new( "EnvironmentalRisk", 0, 10 );
-            environmentalRisk.AddLabel( new FuzzySet( "VeryLow", new TrapezoidalFunction(0, 0, 1, 3) ) );
-            environmentalRisk.AddLabel( new FuzzySet( "Low", new TrapezoidalFunction(1, 3, 5) ) );
-            environmentalRisk.AddLabel( new FuzzySet( "Middle", new TrapezoidalFunction(3, 5, 7) ) );
-            environmentalRisk.AddLabel( new FuzzySet( "High", new TrapezoidalFunction(5, 7, 9) ) );
-            environmentalRisk.AddLabel( new FuzzySet( "VeryHigh", new TrapezoidalFunction(7, 9, 10, 10) ) );
+            LinguisticVariable whiteWater = new( "WhiteWater", 0, 10 );
+            whiteWater.AddLabel( new FuzzySet( "VeryLow", new TrapezoidalFunction(0, 0, 1, 3) ) );
+            whiteWater.AddLabel( new FuzzySet( "Low", new TrapezoidalFunction(1, 3, 5) ) );
+            whiteWater.AddLabel( new FuzzySet( "Middle", new TrapezoidalFunction(3, 5, 7) ) );
+            whiteWater.AddLabel( new FuzzySet( "High", new TrapezoidalFunction(5, 7, 9) ) );
+            whiteWater.AddLabel( new FuzzySet( "VeryHigh", new TrapezoidalFunction(7, 9, 10, 10) ) );
 
             Database fuzzyDB = new();
-            fuzzyDB.AddVariable( solubility );
-            fuzzyDB.AddVariable( toxicity );
-            fuzzyDB.AddVariable( environmentalRisk );
+            fuzzyDB.AddVariable( distilledWaterRecycling );
+            fuzzyDB.AddVariable( reuseOfDistilledWaterInOtherApplications );
+            fuzzyDB.AddVariable( whiteWater );
 
             InferenceSystem IS = new ( fuzzyDB, new CentroidDefuzzifier( 1000 ) );
 
-            IS.NewRule("Rule 1", "IF Solubility IS VeryInsoluble and Toxicity IS VeryLow THEN EnvironmentalRisk IS VeryLow");
-            IS.NewRule("Rule 2", "IF Solubility IS VeryInsoluble and Toxicity IS Low THEN EnvironmentalRisk IS VeryLow");
-            IS.NewRule("Rule 3", "IF Solubility IS VeryInsoluble and Toxicity IS Medium THEN EnvironmentalRisk IS Low");
-            IS.NewRule("Rule 4", "IF Solubility IS VeryInsoluble and Toxicity IS High THEN EnvironmentalRisk IS Low");
-            IS.NewRule("Rule 5", "IF Solubility IS VeryInsoluble and Toxicity IS VeryHigh THEN EnvironmentalRisk IS Middle");
-            IS.NewRule("Rule 6", "IF Solubility IS Insoluble and Toxicity IS VeryLow THEN EnvironmentalRisk IS VeryLow");
-            IS.NewRule("Rule 7", "IF Solubility IS Insoluble and Toxicity IS Low THEN EnvironmentalRisk IS Low");
-            IS.NewRule("Rule 8", "IF Solubility IS Insoluble and Toxicity IS Medium THEN EnvironmentalRisk IS Low");
-            IS.NewRule("Rule 9", "IF Solubility IS Insoluble and Toxicity IS High THEN EnvironmentalRisk IS Middle");
-            IS.NewRule("Rule 10", "IF Solubility IS Insoluble and Toxicity IS VeryHigh THEN EnvironmentalRisk IS High");
-            IS.NewRule("Rule 11", "IF Solubility IS AverageSoluble and Toxicity IS VeryLow THEN EnvironmentalRisk IS Low");
-            IS.NewRule("Rule 12", "IF Solubility IS AverageSoluble and Toxicity IS Low THEN EnvironmentalRisk IS Low");
-            IS.NewRule("Rule 13", "IF Solubility IS AverageSoluble and Toxicity IS Medium THEN EnvironmentalRisk IS Middle");
-            IS.NewRule("Rule 14", "IF Solubility IS AverageSoluble and Toxicity IS High THEN EnvironmentalRisk IS High");
-            IS.NewRule("Rule 15", "IF Solubility IS AverageSoluble and Toxicity IS VeryHigh THEN EnvironmentalRisk IS High");
-            IS.NewRule("Rule 16", "IF Solubility IS Soluble and Toxicity IS VeryLow THEN EnvironmentalRisk IS Low");
-            IS.NewRule("Rule 17", "IF Solubility IS Soluble and Toxicity IS Low THEN EnvironmentalRisk IS Middle");
-            IS.NewRule("Rule 18", "IF Solubility IS Soluble and Toxicity IS Medium THEN EnvironmentalRisk IS High");
-            IS.NewRule("Rule 19", "IF Solubility IS Soluble and Toxicity IS High THEN EnvironmentalRisk IS High");
-            IS.NewRule("Rule 20", "IF Solubility IS Soluble and Toxicity IS VeryHigh THEN EnvironmentalRisk IS VeryHigh");
-            IS.NewRule("Rule 21", "IF Solubility IS VerySoluble and Toxicity IS VeryLow THEN EnvironmentalRisk IS Middle");
-            IS.NewRule("Rule 22", "IF Solubility IS VerySoluble and Toxicity IS Low THEN EnvironmentalRisk IS High");
-            IS.NewRule("Rule 23", "IF Solubility IS VerySoluble and Toxicity IS Medium THEN EnvironmentalRisk IS High");
-            IS.NewRule("Rule 24", "IF Solubility IS VerySoluble and Toxicity IS High THEN EnvironmentalRisk IS VeryHigh");
-            IS.NewRule("Rule 25", "IF Solubility IS VerySoluble and Toxicity IS VeryHigh THEN EnvironmentalRisk IS VeryHigh");
+            IS.NewRule("Rule 1", "IF DistilledWaterRecycling IS VeryLow and ReuseOfDistilledWaterInOtherApplications IS VeryLow THEN WhiteWater IS VeryLow");
+            IS.NewRule("Rule 2", "IF DistilledWaterRecycling IS VeryLow and ReuseOfDistilledWaterInOtherApplications IS Low THEN WhiteWater IS VeryLow");
+            IS.NewRule("Rule 3", "IF DistilledWaterRecycling IS VeryLow and ReuseOfDistilledWaterInOtherApplications IS Medium THEN WhiteWater IS Low");
+            IS.NewRule("Rule 4", "IF DistilledWaterRecycling IS VeryLow and ReuseOfDistilledWaterInOtherApplications IS High THEN WhiteWater IS Low");
+            IS.NewRule("Rule 5", "IF DistilledWaterRecycling IS VeryLow and ReuseOfDistilledWaterInOtherApplications IS VeryHigh THEN WhiteWater IS Middle");
+            IS.NewRule("Rule 6", "IF DistilledWaterRecycling IS Low and ReuseOfDistilledWaterInOtherApplications IS VeryLow THEN WhiteWater IS VeryLow");
+            IS.NewRule("Rule 7", "IF DistilledWaterRecycling IS Low and ReuseOfDistilledWaterInOtherApplications IS Low THEN WhiteWater IS Low");
+            IS.NewRule("Rule 8", "IF DistilledWaterRecycling IS Low and ReuseOfDistilledWaterInOtherApplications IS Medium THEN WhiteWater IS Low");
+            IS.NewRule("Rule 9", "IF DistilledWaterRecycling IS Low and ReuseOfDistilledWaterInOtherApplications IS High THEN WhiteWater IS Middle");
+            IS.NewRule("Rule 10", "IF DistilledWaterRecycling IS Low and ReuseOfDistilledWaterInOtherApplications IS VeryHigh THEN WhiteWater IS High");
+            IS.NewRule("Rule 11", "IF DistilledWaterRecycling IS Middle and ReuseOfDistilledWaterInOtherApplications IS VeryLow THEN WhiteWater IS Low");
+            IS.NewRule("Rule 12", "IF DistilledWaterRecycling IS Middle and ReuseOfDistilledWaterInOtherApplications IS Low THEN WhiteWater IS Low");
+            IS.NewRule("Rule 13", "IF DistilledWaterRecycling IS Middle and ReuseOfDistilledWaterInOtherApplications IS Medium THEN WhiteWater IS Middle");
+            IS.NewRule("Rule 14", "IF DistilledWaterRecycling IS Middle and ReuseOfDistilledWaterInOtherApplications IS High THEN WhiteWater IS High");
+            IS.NewRule("Rule 15", "IF DistilledWaterRecycling IS Middle and ReuseOfDistilledWaterInOtherApplications IS VeryHigh THEN WhiteWater IS High");
+            IS.NewRule("Rule 16", "IF DistilledWaterRecycling IS High and ReuseOfDistilledWaterInOtherApplications IS VeryLow THEN WhiteWater IS Low");
+            IS.NewRule("Rule 17", "IF DistilledWaterRecycling IS High and ReuseOfDistilledWaterInOtherApplications IS Low THEN WhiteWater IS Middle");
+            IS.NewRule("Rule 18", "IF DistilledWaterRecycling IS High and ReuseOfDistilledWaterInOtherApplications IS Medium THEN WhiteWater IS High");
+            IS.NewRule("Rule 19", "IF DistilledWaterRecycling IS High and ReuseOfDistilledWaterInOtherApplications IS High THEN WhiteWater IS High");
+            IS.NewRule("Rule 20", "IF DistilledWaterRecycling IS High and ReuseOfDistilledWaterInOtherApplications IS VeryHigh THEN WhiteWater IS VeryHigh");
+            IS.NewRule("Rule 21", "IF DistilledWaterRecycling IS VeryHigh and ReuseOfDistilledWaterInOtherApplications IS VeryLow THEN WhiteWater IS Middle");
+            IS.NewRule("Rule 22", "IF DistilledWaterRecycling IS VeryHigh and ReuseOfDistilledWaterInOtherApplications IS Low THEN WhiteWater IS High");
+            IS.NewRule("Rule 23", "IF DistilledWaterRecycling IS VeryHigh and ReuseOfDistilledWaterInOtherApplications IS Medium THEN WhiteWater IS High");
+            IS.NewRule("Rule 24", "IF DistilledWaterRecycling IS VeryHigh and ReuseOfDistilledWaterInOtherApplications IS High THEN WhiteWater IS VeryHigh");
+            IS.NewRule("Rule 25", "IF DistilledWaterRecycling IS VeryHigh and ReuseOfDistilledWaterInOtherApplications IS VeryHigh THEN WhiteWater IS VeryHigh");
 
-            IS.SetInput("Solubility", (float)solubilityValue);
-            IS.SetInput("Toxicity", (float)toxicityValue);
+            IS.SetInput("DistilledWaterRecycling", (float)distilledWaterRecyclingValue);
+            IS.SetInput("ReuseOfDistilledWaterInOtherApplications", (float)reuseOfDistilledWaterInOtherApplicationsValue);
 
-            double resultado = IS.Evaluate("EnvironmentalRisk");
+            double resultado = IS.Evaluate("WhiteWater");
 
             double[] input = new double[2];
             for (int i = 0; i < 2; i++)
             {
-                IS.SetInput("Solubility", i == 0 ? (float)32.99 : 0);
-                IS.SetInput("Toxicity", i == 0 ? (float)44.99 : 0);
-                input[i] = IS.Evaluate("EnvironmentalRisk");
+                IS.SetInput("DistilledWaterRecycling", i == 0 ? (float)9.99 : 0);
+                IS.SetInput("ReuseOfDistilledWaterInOtherApplications", i == 0 ? (float)9.99 : 0);
+                input[i] = IS.Evaluate("WhiteWater");
             }
-            double m = (IS.GetLinguisticVariable("EnvironmentalRisk").End - IS.GetLinguisticVariable("EnvironmentalRisk").Start) / (input[1] - input[0]);
-            resultado = m * (resultado - input[1]) + IS.GetLinguisticVariable("EnvironmentalRisk").End;
+            double m = (IS.GetLinguisticVariable("WhiteWater").End - IS.GetLinguisticVariable("WhiteWater").Start) / (input[1] - input[0]);
+            resultado = m * (resultado - input[1]) + IS.GetLinguisticVariable("WhiteWater").End;
             
             if (resultado >= 0 && resultado <= 2.5)
             {
